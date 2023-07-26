a1f0e3807f178643f11b6bacc98663d4aa417ecc
Arima1213 <arima3992@gmail.com>
2023-07-26 23:37:42 +0700

    bar8

--- src/data/works.jsx
+++ src/data/works.jsx
@@ -2,28 +2,28 @@ export default [
 	{
 		title: 'News Blog',
 		imgUrl:
-			'../images/Frame1.png',
+			'src/images/Frame1.png',
 		tech: ['React JS', 'Tailwind CSS'],
 		workUrl: 'https://projek-invest-like-a-pro.vercel.app/',
 	},
 	{
 		title: "Donations Blog",
 		imgUrl:
-			'../images/Frame2.png',
+			'src/images/Frame2.png',
 		tech: ['HTML', 'Bootstrap', 'PHP'],
 		workUrl: 'https://webtakafuldonations.000webhostapp.com/',
 	},
 	{
 		title: "Profile Company Website",
 		imgUrl:
-			'../images/Frame3.png',
+			'src/images/Frame3.png',
 		tech: ['Tailwind CSS', 'Laravel'],
 		workUrl: '',
 	},
 	{
 		title: 'Zona Tani Ui/UX Design',
 		imgUrl:
-			'../images/Frame4.png',
+			'src/images/Frame4.png',
 		tech: ['Figma', 'Design Thinking'],
 		workUrl: '',
 	},
